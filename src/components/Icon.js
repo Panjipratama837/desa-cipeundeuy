83a5ddf98a9b6057720fe609a486e51b6a830336
Panjipratama837 <panjipratama1984@gmail.com>
2022-03-10 23:20:36 +0700

    Add Copy link to clipboard

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./Icon.css";
 import {
   FacebookShareButton,
@@ -6,88 +8,44 @@ import {
   WhatsappIcon,
   TwitterShareButton,
   TwitterIcon,
-  TelegramShareButton,
-  TelegramIcon,
-  LinkedinIcon,
-  LinkedinShareButton,
-  LineShareButton,
-  LineIcon,
-  EmailShareButton,
-  EmailIcon,
-  RedditShareButton,
-  RedditIcon,
 } from "react-share";
+
+import { CopyToClipboard } from "react-copy-to-clipboard";
 
 function Icon() {
   // const currentUrl = "https://www.youtube.com/watch?v=2BnTYEafRQc&t=91s";
   const currentUrl = window.location.href;
   console.log(currentUrl);
 
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 300);
+  };
+
   return (
     <div className="icon d-flex justify-content-end">
-      <i
-        className="bi bi-link text-black icon-item"
-        data-bs-toggle="modal"
-        data-bs-target="#other-share"
-      ></i>
+      <CopyToClipboard text={currentUrl} onCopy={handleCopy}>
+        {copied ? (
+          <i className="bi bi-check2 icon-item"></i>
+        ) : (
+          <i className="bi bi-clipboard icon-item"></i>
+        )}
+      </CopyToClipboard>
 
-      <div
-        className="modal fade "
-        id="other-share"
-        tabIndex="-1"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div className="modal-dialog modal-lg modal-dialog-centered">
-          <div className="modal-content">
-            <div className="modal-header">
-              <h5 className="modal-title" id="exampleModalLabel">
-                Share Other Social Media
-              </h5>
-            </div>
-            <div className="modal-body d-flex justify-content-center">
-              <RedditShareButton url={currentUrl} className="icon-item">
-                <RedditIcon size={45} round />
-              </RedditShareButton>
-
-              <EmailShareButton url={currentUrl} className="icon-item">
-                <EmailIcon size={45} round />
-              </EmailShareButton>
-
-              <LinkedinShareButton url={currentUrl} className="icon-item">
-                <LinkedinIcon size={45} round />
-              </LinkedinShareButton>
-
-              <LineShareButton url={currentUrl} className="icon-item">
-                <LineIcon size={45} round />
-              </LineShareButton>
-
-              <TelegramShareButton url={currentUrl} className="icon-item">
-                <TelegramIcon size={45} round />
-              </TelegramShareButton>
-            </div>
-            <div className="modal-footer">
-              <button
-                type="button"
-                className="btn btn-secondary"
-                data-bs-dismiss="modal"
-              >
-                Close
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
       <FacebookShareButton url={currentUrl} className="icon-item">
-        <FacebookIcon size={45} round />
+        <FacebookIcon size={35} round />
       </FacebookShareButton>
 
       <TwitterShareButton url={currentUrl} className="icon-item">
-        <TwitterIcon size={45} round />
+        <TwitterIcon size={35} round />
       </TwitterShareButton>
 
       <WhatsappShareButton url={currentUrl} className="icon-item">
-        <WhatsappIcon size={45} round />
+        <WhatsappIcon size={35} round />
       </WhatsappShareButton>
     </div>
   );
